cc430b9016b02a167b24c3e955f6dab2a3ef0c95
Fcscanf <fcscanf@gmail.com>
2019-03-02 12:35:37 +0800

    C++-重载模板函数

--- main.cpp
+++ main.cpp
@@ -340,9 +340,33 @@ void Swap(Any &a, Any &b){
     b = temp;
 }
 
+// 重载的模板
+template<class Any>
+void Swap(Any *a, Any *b, int n);
+void Show(int a[]);
+const int Lim = 8;
+
+template<class Any>
+void Swap(Any a[], Any b[], int n){
+    Any temp;
+    for (int i = 0; i < n; i++) {
+        temp = a[i];
+        a[i] = b[i];
+        b[i] = temp;
+    }
+}
+
+void Show(int a[]){
+    cout << a[0] << a[1] << endl;
+    cout << a[2] << a[3] << endl;
+    for (int i = 4; i < Lim; ++i) {
+        cout << a[i];
+    }
+    cout << endl;
+}
+
 void useTemSwap(){
-    int i = 10;
-    int j = 20;
+    int i = 10, j = 20;
     cout << "i, j =" << i << "," << j << endl;
     Swap(i, j);
     cout << "i, j =" << i << "," << j << endl;
@@ -351,6 +375,16 @@ void useTemSwap(){
     cout << "n, m =" << n << "," << m << endl;
     Swap(n, m);
     cout << "n, m =" << n << "," << m << endl;
+
+    int d1[Lim] = {0, 7, 0, 4, 1, 7, 7, 6};
+    int d2[Lim] = {0, 6, 2, 0, 2, 9, 6, 9};
+    cout << "Original arrays: \n";
+    Show(d1);
+    Show(d2);
+    Swap(d1, d2, Lim);
+    cout << "Swapped arrays: \n";
+    Show(d1);
+    Show(d2);
 }
 
 int main() {
